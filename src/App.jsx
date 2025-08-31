e68e732a138408557b30aa5a909ccca7b05f7ee3
Camilo Marín Naranjo <naranjocamilo26@gmail.com>
2025-08-31 01:12:27 -0500

    git add: configuración docker y inicio del header

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 import './styles/tailwindConfigs.css';
-
-function App() {
+import Header from './components/Header';
+export default function App() {
   const [visible, setVisible] = useState(true);
   return (
     <>
-      <h1>Hungaro's Debts Management</h1>
-      <div className="card">
-        <button onClick={() => setVisible(!visible)}>
-          {visible ? "Ocultar" : "Mostrar"}
-        </button>
-        <p>
-          {visible && <p>¡Hola! Soy un texto condicional 👋</p>}
-        </p>
-      </div>
+      <Header/>
     </>
   )
 }
-
-export default App
